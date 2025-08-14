9d90379aac1ebd73d2ec4f5123017a75e156ee21
chvrif001 <111813903+chvrif001@users.noreply.github.com>
2025-08-14 18:49:41 +0200

    Add files via upload

---
+++ Practical_1B_Trial.cpp
@@ -0,0 +1,305 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+ 
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdint.h>
+#include "stm32f0xx.h"
+/* USER CODE END Includes */
+ 
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+#define MAX_ITER 100
+/* USER CODE END PTD */
+ 
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+ 
+/* USER CODE END PD */
+ 
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+ 
+/* USER CODE END PM */
+ 
+/* Private variables ---------------------------------------------------------*/
+ 
+/* USER CODE BEGIN PV */
+//TODO: Define and initialise the global varibales required
+//const int image_sizes[] = {128, 160, 192, 224, 256};
+uint64_t checksum = 0;
+uint32_t start_time = 0;
+uint32_t end_time = 0;
+uint32_t execution_time;
+/*
+  start_time
+  end_time
+  execution_time
+  checksum: should be uint64_t
+  initial width and height maybe or you might opt for an array??
+*/
+ 
+/* USER CODE END PV */
+ 
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+uint64_t calculate_mandelbrot_fixed_point_arithmetic(int width, int height, int max_iterations);
+uint64_t calculate_mandelbrot_double(int width, int height, int max_iterations);
+ 
+ 
+/* USER CODE END PFP */
+ 
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+ 
+/* USER CODE END 0 */
+ 
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+ 
+  /* USER CODE END 1 */
+ 
+  /* MCU Configuration--------------------------------------------------------*/
+ 
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+ 
+  /* USER CODE BEGIN Init */
+ 
+  /* USER CODE END Init */
+ 
+  /* Configure the system clock */
+  SystemClock_Config();
+ 
+  /* USER CODE BEGIN SysInit */
+ 
+  /* USER CODE END SysInit */
+ 
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+  //TODO: Turn on LED 0 to signify the start of the operation
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
+ 
+  //TODO: Record the start time
+  start_time = HAL_GetTick();
+  
+  //TODO: Call the Mandelbrot Function and store the output in the checksum variable defined initially
+  checksum = calculate_mandelbrot_double(160, 160, MAX_ITER);
+ 
+  //TODO: Record the end time
+  end_time = HAL_GetTick();
+ 
+  //TODO: Calculate the execution time
+  execution_time = end_time - start_time; //Execution time is just the difference between the start and end time//
+ 
+  //TODO: Turn on LED 1 to signify the end of the operation
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
+ 
+  //TODO: Hold the LEDs on for a 1s delay
+  HAL_Delay(1000);
+ 
+  //TODO: Turn off the LEDs
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+ 
+  /* USER CODE END 2 */
+ 
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+ 
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+ 
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+ 
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+ 
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+ 
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+ 
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+ 
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+ 
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_1, GPIO_PIN_RESET);
+ 
+  /*Configure GPIO pins : PB0 PB1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+ 
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+ 
+/* USER CODE BEGIN 4 */
+//TODO: Mandelbroat using variable type integers and fixed point arithmetic
+uint64_t calculate_mandelbrot_fixed_point_arithmetic(int width, int height, int max_iterations){
+    const int64_t MULT = 1000000LL; // fixed-point multiplier
+    const int64_t MULT_2P0 = 2 * MULT;
+    const int64_t MULT_3P5 = 7 * MULT / 2;   // 3.5 * MULT
+    const int64_t MULT_2P5 = 5 * MULT / 2;   // 2.5 * MULT
+    const int64_t MULT_4P0 = 4 * MULT;
+    uint64_t mandelbrot_sum = 0;
+    //TODO: Complete the function implementation
+    for (int y = 0; y < height; y++) {
+            int64_t y0 = ((int64_t)y * MULT_2P0) / height - MULT; // (y/height)*2.0 - 1.0
+            for (int x = 0; x < width; x++) {
+                int64_t x0 = ((int64_t)x * MULT_3P5) / width - MULT_2P5;
+ 
+                int64_t xi = 0;
+                int64_t yi = 0;
+                int iteration = 0;
+ 
+                while (iteration < max_iterations) {
+                    int64_t xi_sq = (xi * xi) / MULT;
+                    int64_t yi_sq = (yi * yi) / MULT;
+ 
+                    if ((xi_sq + yi_sq) > MULT_4P0) break;
+ 
+                    int64_t xtemp = xi_sq - yi_sq;
+                    yi = ((2 * xi * yi) / MULT) + y0;
+                    xi = xtemp + x0;
+ 
+                    iteration++;
+                }
+ 
+                mandelbrot_sum += iteration; //THIS IS THE CHECKSUM 
+            }
+        }
+    return mandelbrot_sum;
+ 
+}
+ 
+//TODO: Mandelbroat using variable type double
+uint64_t calculate_mandelbrot_double(int width, int height, int max_iterations){
+    uint64_t mandelbrot_sum = 0;
+    //TODO: Complete the function implementation
+    for (int y = 0; y < height; y++) {
+            double y0 = ((double)y * 2.0 / (double)height) - 1.0;
+            for (int x = 0; x < width; x++) {
+                double x0 = ((double)x * 3.5 / (double)width) - 2.5;
+ 
+                double xi = 0.0;
+                double yi = 0.0;
+                int iteration = 0;
+ 
+                while (iteration < max_iterations && (xi * xi + yi * yi <= 4.0)) {
+                    double xtemp = xi * xi - yi * yi;
+                    yi = 2.0 * xi * yi + y0;
+                    xi = xtemp + x0;
+                    iteration++;
+                }
+ 
+                mandelbrot_sum += iteration;
+            }
+        }
+    return mandelbrot_sum;
+}
+ 
+/* USER CODE END 4 */
+ 
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+ 
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
